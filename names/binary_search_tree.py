136963a6b0f34bdbb9f8e346b9685bea42e35517
Ashley Dunham <xenocide21@gmail.com>
2020-08-28 16:30:41 -0700

    names.py Complete, added binary_search_tree.py from Data Structures Project.

---
+++ names/binary_search_tree.py
@@ -0,0 +1,52 @@
+class BinarySearchTree:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value is target:
+            return True
+        elif self.value > target:
+            if self.left is not None:
+                return self.left.contains(target)
+            else:
+                return False
+        else:
+            if self.right is not None:
+                return self.right.contains(target)
+            else:
+                return False
+
+    def get_max(self):
+        if self.right is not None:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left is not None:
+            self.left.for_each(cb)
+        if self.right is not None:
+            self.right.for_each(cb)
+
+    def in_order_print(self):
+        if self.left:
+            self.left.in_order_print(self.left)
+        print(self.value)
+        if self.right:
+            self.right.in_order_print(self.right)
--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -13,10 +14,22 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+"""runtime: 9.506585597991943 seconds"""
+
+bst = BinarySearchTree("")
+for name_2 in names_2:
+    bst.insert(name_2)
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if bst.contains(name_1):
+        duplicates.append(name_1)
+
+"""runtime: 0.1349499225616455 seconds"""
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")

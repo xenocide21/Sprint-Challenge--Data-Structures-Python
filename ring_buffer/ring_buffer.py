02373070cd7be82620e0947cf45be0aca6601ae1
Ashley Dunham <xenocide21@gmail.com>
2020-08-28 16:12:54 -0700

    Ring Buffer Complete, added doubly_linked_list.py from Data Structures Project.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,27 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+        else:
+            if self.current is None or self.current.next is None:
+                self.current = self.storage.head
+            else:
+                self.current = self.current.next
+            self.current.value = item
 
     def get(self):
-        pass
+        list_buffer_contents = []
+        node = self.storage.head
+        while node is not None:
+            list_buffer_contents.append(node.value)
+            node = node.next
+
+        return list_buffer_contents
